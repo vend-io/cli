8d297e71df738a354d5d0a4324150e99a37647bf
iwatakeshi <iwatakeshi@gmail.com>
2016-09-28 19:29:07 -0600

    Update: WIP :wrench:
    Added REPL

---
+++ CLI/Sources/REPL/REPL.cs
@@ -0,0 +1,44 @@
+﻿//
+// REPL.cs
+//
+// Author:
+//       takeshi <>
+//
+// Copyright (c) 2016 ${CopyrightHolder}
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+namespace CLI {
+	public class REPL : IREPL {
+		public REPL() {
+
+		}
+		public void Read(string[] args) {
+
+		}
+
+		public void Eval() {
+
+		}
+
+		public void Print() {
+
+		}
+	}
+}
